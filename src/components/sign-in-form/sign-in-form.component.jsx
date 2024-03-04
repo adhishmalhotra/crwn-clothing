abf82db8638cedd482d789c06daccceaab453728
adhishmalhotra <adhish.malhotra@ontariotechu.net>
2024-03-04 13:04:23 -0500

    completed section 111

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import {useState} from 'react'
+import {useState, useContext} from 'react'
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+
+import { UserContext } from '../../contexts/user.context';
 
 
 import { signInWithGooglePopup, 
@@ -19,7 +21,7 @@ const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields); 
     const {email, password} = formFields; 
 
-    // console.log(formFields);
+    const { setCurrentUser } = useContext(UserContext);
 
     const resetFormFields = () => {
       setFormFields(defaultFormFields);
@@ -35,10 +37,9 @@ const SignInForm = () => {
 
 
       try{
-        const response = await signInAuthUserWithEmailAndPassword(email, password); 
-        console.log(response); 
+        const {user} = await signInAuthUserWithEmailAndPassword(email, password); 
         resetFormFields();
-
+        
       } catch(error) {
         switch (error.code){
           case 'auth/wrong-password': alert("incorrect password for email");
